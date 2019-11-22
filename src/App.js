fb71668ca0e42dbbf138607d7488f9a9fd7e2dd7
Ernur Kurmanbekov <erazavr85@gmail.com>
2019-11-22 11:13:32 +0600

    Wrote methods addIngr and removeIngr

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component,Fragment} from 'react';
+import Ingredient from "./components/Ingredients/Ingredients";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import RenderBurger from "./components/Burger/RenderBurger";
+import './App.css'
+
+class App extends Component {
+
+    state = {
+                ingredients: [
+            {name: 'Meat', count: 0,},
+            {name: 'Cheese', count: 0,},
+            {name: 'Bacon', count: 0,},
+            {name: 'Salad', count: 0,},
+        ],
+        totalPrice: 20,
+    };
+    addIngr = obj => {
+        const ingredients = [...this.state.ingredients];
+        const ingredientsIndex = ingredients.findIndex(t => t.name === obj.name);
+        ingredients[ingredientsIndex].count++;
+        let totalPrice = this.state.totalPrice;
+        totalPrice += obj.price;
+        this.setState({ingredients,totalPrice})
+    };
+    removeIngr = name => {
+        const ingredients = [...this.state.ingredients];
+        const ingredientsIndex = ingredients.findIndex(t => t.name === name.name);
+        ingredients[ingredientsIndex].count--;
+        let totalPrice = this.state.totalPrice;
+        totalPrice -= name.price;
+        this.setState({ingredients,totalPrice})
+    };
+
+
+  render() {
+    return (
+        <Fragment>
+            <div style={{marginTop: '10px',textAlign: 'center'}}>
+        <div className='mainBlock'>
+          <h4>Ingredients</h4>
+                <Ingredient
+                  ingredientsApp={this.state.ingredients}
+                  addIngredient={this.addIngr}
+                  remove={this.removeIngr}
+
+                />
+        </div>
+                <RenderBurger
+                    ingredients={this.state.ingredients}
+                    total={this.state.totalPrice}/>
+            </div>
+        </Fragment>
+    );
+  }
 }
 
 export default App;
